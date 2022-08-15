02ad417dcb5305b23814e600c89df7b0aa2dcc84
sisfosph <85800915+ariv-me@users.noreply.github.com>
2022-08-15 15:41:25 +0700

    kategori

---
+++ resources/views/blank.blade.php
@@ -0,0 +1,19 @@
+@extends('apps.layouts.template')
+
+@section('content') 
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="alert alert-info border-0" role="alert">
+                            <strong> MODUL BELUM ADA </strong> 
+                        </div>
+                    </div>
+                </div>                                                                              
+            </div> 
+        </div>
+    </div>                             
+</div>
+@endsection
